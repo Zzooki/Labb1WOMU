fc9e5d78aa9886851556a0c03b87a4eb5b95d717
Zzooki <zzoooki@gmail.com>
2016-01-26 14:52:58 +0100

    hejsan

---
+++ Labb1WOMU/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Labb1WOMU.Startup))]
+namespace Labb1WOMU
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
