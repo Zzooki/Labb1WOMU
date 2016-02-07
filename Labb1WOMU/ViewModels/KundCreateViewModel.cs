95f0ea6c4d91840cdea0169408cc040e85372031
Zzooki <zzoooki@gmail.com>
2016-02-07 16:09:39 +0100

    Kommenterat

--- Labb1WOMU/ViewModels/KundCreateViewModel.cs
+++ Labb1WOMU/ViewModels/KundCreateViewModel.cs
@@ -5,6 +5,8 @@ using System.Web;
 
 namespace Labb1WOMU.ViewModels
 {
+    /// Denna viewmodell hanterar informationen för att skicka meddelande till kunden angående 
+    /// för felinmatningar i registreringsprocessen i samband med utcheckningen utav varukorgen
     public class KundCreateViewModel
     {
         public string Message { get; set; }
