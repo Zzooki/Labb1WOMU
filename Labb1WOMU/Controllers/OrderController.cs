37589f6ae9797a07fadaaf1759bf1e02b471fec3
Zzooki <zzoooki@gmail.com>
2016-02-07 17:09:18 +0100

    hej

--- Labb1WOMU/Controllers/OrderController.cs
+++ Labb1WOMU/Controllers/OrderController.cs
@@ -14,12 +14,7 @@ namespace Labb1WOMU.Controllers
     {
         private DBTEntities1 db = new DBTEntities1();
 
-        // GET: Order
-        public ActionResult Index()
-        {
-            var order = db.Order.Include(o => o.Kund);
-            return View(order.ToList());
-        }
+       
         
         public ActionResult Search(string searchValue)
         {
@@ -40,107 +35,6 @@ namespace Labb1WOMU.Controllers
 
             return View(result);
 
-        }
-
-
-
-        // GET: Order/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Order order = db.Order.Find(id);
-            if (order == null)
-            {
-                return HttpNotFound();
-            }
-            return View(order);
-        }
-
-        // GET: Order/Create
-        public ActionResult Create()
-        {
-            ViewBag.KundID = new SelectList(db.Kund, "KundID", "Förnamn");
-            return View();
-        }
-
-        // POST: Order/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "OrderId,KundID")] Order order)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Order.Add(order);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.KundID = new SelectList(db.Kund, "KundID", "Förnamn", order.KundID);
-            return View(order);
-        }
-
-        // GET: Order/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Order order = db.Order.Find(id);
-            if (order == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.KundID = new SelectList(db.Kund, "KundID", "Förnamn", order.KundID);
-            return View(order);
-        }
-
-        // POST: Order/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "OrderId,KundID")] Order order)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(order).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.KundID = new SelectList(db.Kund, "KundID", "Förnamn", order.KundID);
-            return View(order);
-        }
-
-        // GET: Order/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Order order = db.Order.Find(id);
-            if (order == null)
-            {
-                return HttpNotFound();
-            }
-            return View(order);
-        }
-
-        // POST: Order/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Order order = db.Order.Find(id);
-            db.Order.Remove(order);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
