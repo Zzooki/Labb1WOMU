4d055d5855915ae9a0dfd4bef7be68aa1487c9cb
RasmusLundquist <Rasmus.lundquist@hotmail.com>
2016-02-07 17:06:50 +0100

    Kommentarer

--- Labb1WOMU/Controllers/ArtikelController.cs
+++ Labb1WOMU/Controllers/ArtikelController.cs
@@ -15,12 +15,27 @@ namespace Labb1WOMU.Controllers
         private DBTEntities1 db = new DBTEntities1();
 
         // GET: Artikel
+        /// <summary>
+        /// Denna metod returnerar en view med samtliga artiklar i databasen.
+        /// </summary>
+        /// <returns>
+        /// Returnerar en vy med samtliga artiklar.
+        /// </returns>
         public ActionResult Index()
         {
             return View(db.Artikel.ToList());
         }
 
         // GET: Artikel/Details/5
+        /// <summary>
+        /// Denna metod används för att ta fram en detaljerad vy för en given artikel.
+        /// </summary>
+        /// <param name="id">
+        /// Idt för artikeln som en detaljerad vy skall visas för.
+        /// </param>
+        /// <returns>
+        /// Returnerar den detaljerade vyn för den valda produkten.
+        /// </returns>
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -44,6 +59,15 @@ namespace Labb1WOMU.Controllers
         // POST: Artikel/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Denna metod används för att lägga till en ny produkt i databasen.
+        /// </summary>
+        /// <param name="artikel">
+        /// Metoden tar in information om en produkt från ett webbformulär och skapar en artikel instans med den
+        /// datan som sedan sparas till databasen.</param>
+        /// <returns>
+        /// Metoden returnerar index vyn för artikel om artikeln lyckades skapas annars får användaren en ny chans att mata
+        /// in produkten.</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ArtikelID,ArtikelNamn,Beskrivning,Antal,Pris,Aktuell")] Artikel artikel)
@@ -76,6 +100,17 @@ namespace Labb1WOMU.Controllers
         // POST: Artikel/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Denna metod används för att ändra på informationen om en artikel som finns i databasen.
+        /// Detta görs med information som matas in av användare genom ett webbförmulär.
+        /// </summary>
+        /// <param name="artikel">
+        /// En artikel instans skapas med information från ett webbformulär, sedan sparas artikelinstansen till databasen.
+        /// </param>
+        /// <returns>
+        /// Metoden returnerar artikel/index vyn om artikeln ändrades utan fel, och annars får användren en ny chans
+        /// att göra ändringarna.
+        /// </returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "ArtikelID,ArtikelNamn,Beskrivning,Antal,Pris,Aktuell")] Artikel artikel)
@@ -105,6 +140,15 @@ namespace Labb1WOMU.Controllers
         }
 
         // POST: Artikel/Delete/5
+        /// <summary>
+        /// Denna metod används för att ta bort en artikel från databasen.
+        /// </summary>
+        /// <param name="id">
+        /// Idt på produkten som skall tas bort från databasen.
+        /// </param>
+        /// <returns>
+        /// Returnerar index metoden för klassen.
+        /// </returns>
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
--- Labb1WOMU/Controllers/OrderController.cs
+++ Labb1WOMU/Controllers/OrderController.cs
@@ -15,12 +15,27 @@ namespace Labb1WOMU.Controllers
         private DBTEntities1 db = new DBTEntities1();
 
         // GET: Order
+        /// <summary>
+        /// Denna metod skapar en vy för att visa alla ordrar som tillhör en given kund.
+        /// </summary>
+        /// <returns>
+        /// Returnerar en vy som visar de ordrar som tillhör kuden.
+        /// </returns>
         public ActionResult Index()
         {
             var order = db.Order.Include(o => o.Kund);
             return View(order.ToList());
         }
         
+        /// <summary>
+        /// Denna metod används för att söka efter ordrar i databasen.
+        /// </summary>
+        /// <param name="searchValue">
+        /// Idt på den order som användaren matat in.
+        /// </param>
+        /// <returns>
+        /// returnerar en vy med den order som användaren matat in, eller en tom vy om ingen order hittats.
+        /// </returns>
         public ActionResult Search(string searchValue)
         {
      
@@ -45,6 +60,16 @@ namespace Labb1WOMU.Controllers
 
 
         // GET: Order/Details/5
+        /// <summary>
+        /// Denna metod används för att skapa en detaljerad vy över en viss order.
+        /// </summary>
+        /// <param name="id">
+        /// Idt på ordern som skall detaljer skall visas om.
+        /// </param>
+        /// <returns>
+        /// Returnerar en detaljerad vy över den order som matats in, eller en vy som indikerar att orden inte
+        /// hittats om ingen order hittats.
+        /// </returns>
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -69,6 +94,16 @@ namespace Labb1WOMU.Controllers
         // POST: Order/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Denna metod används för att skapa en order.
+        /// </summary>
+        /// <param name="order">
+        /// Metoden tar in information från ett webbformulär och skapar en order instans med den datan, och 
+        /// det objektet sparas sedan till databasen.</param>
+        /// <returns>
+        /// Returnerar en vy med den skapade ordern om det gick att skapa den, om det inte gick
+        /// så får användaren en ny chans att fylla i formuläret.
+        /// </returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "OrderId,KundID")] Order order)
@@ -103,6 +138,17 @@ namespace Labb1WOMU.Controllers
         // POST: Order/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Denna metod används för att ändra på en order som redan finns sparad i databasen.
+        /// </summary>
+        /// <param name="order">
+        /// Tar in information som skall sparas till existerande order från ett webbformulär detta används
+        /// för att skapa en order instans som sedan används för att uppdaterade den redan existerande ordern.
+        /// </param>
+        /// <returns>
+        /// Metoden returnerar index metoden i klassen om ändringen lyckades, annars får användaren en ny chans att
+        /// fylla i formuläret.
+        /// </returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "OrderId,KundID")] Order order)
@@ -133,6 +179,15 @@ namespace Labb1WOMU.Controllers
         }
 
         // POST: Order/Delete/5
+        /// <summary>
+        /// Denna metod används för att ta bort en order som finns i databasen.
+        /// </summary>
+        /// <param name="id">
+        /// Idt på ordern som skall tas bort.
+        /// </param>
+        /// <returns>
+        /// Metoden returnerar klassens index metod.
+        /// </returns>
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
